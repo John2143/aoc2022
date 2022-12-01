a888bd720c01ea8a2d9c84785d102e247618bf3d
John Schmidt <john@john2143.com>
2022-12-01 09:34:58 -0500

    day 1 improved

--- src/code/p1.rs
+++ src/code/p1.rs
@@ -6,11 +6,18 @@ use itertools::Itertools;
 #[derive(Debug)]
 struct Elf(i32);
 
+pub fn lines() -> impl Iterator<Item = &'static str> {
+    include_str!("../data/p1.txt").lines()
+}
+
 pub fn read_data() -> impl Iterator<Item = Option<i32>> {
-    include_str!("../data/p1.txt").lines().map(|x| match x {
-        "" => None,
-        x => Some(x.parse::<i32>().unwrap()),
-    })
+    include_str!("../data/p1.txt")
+        .lines()
+        .map(|line| match line {
+            "" => None,
+            n => Some(n.parse::<i32>().unwrap()),
+        })
+        .chain(std::iter::repeat(None))
 }
 
 fn get_elves() -> Vec<Elf> {
@@ -28,23 +35,88 @@ fn get_elves() -> Vec<Elf> {
         }
     }
 
-    elves.push(cur_elf);
-
     elves
 }
 
 pub fn a() {
-    let elves = get_elves();
-    let max_cal = elves.into_iter().map(|elf| elf.0).max().unwrap();
-    println!("Max calories: {max_cal}");
+    let elves = get_elves_2();
+    let max_cal = elves.map(|elf| elf.0).max().unwrap();
+    println!("Part 1: {max_cal}");
 }
 
 pub fn b() {
-    let mut elves = get_elves();
+    let mut elves = get_elves_2().collect_vec();
 
-    elves.sort_by_key(|elf| elf.0);
+    elves.sort_by_key(|elf| -elf.0);
 
-    let top_three = &elves[(elves.len() - 3)..];
-    let top_three_cal: i32 = top_three.iter().map(|elf| elf.0).sum();
-    println!("Max calories top three: {top_three_cal}");
+    let top_three_cal: i32 = elves[0..3].iter().map(|elf| elf.0).sum();
+    println!("Part 2: {top_three_cal}");
+}
+
+/*
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+struct ElfIter<I>(I);
+
+impl<I> Iterator for ElfIter<I>
+where
+    I: Iterator<Item = Option<i32>>,
+{
+    type Item = Elf;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut tot = 0;
+        while let Some(Some(v)) = self.0.next() {
+            tot += v;
+        }
+
+        match tot {
+            0 => None,
+            n => Some(Elf(n)),
+        }
+    }
+}
+
+fn get_elves_2() -> impl Iterator<Item = Elf> {
+    ElfIter(read_data())
 }
